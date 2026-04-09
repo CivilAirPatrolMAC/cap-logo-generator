c5d0f3c89b7bbf157f02b04aa0444a83e848c373
Roman Vitanza <rvitanza@cap.gov>
2026-04-08 21:43:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -509,7 +509,7 @@ ctx.miterLimit = 2;
 ctx.font = '700 ' + fontSize + 'px Rajdhani';
 
 const textWidth = measureTrackedText(text, tracking);
-const startX = wordmarkLeft + 20;
+const startX = wordmarkLeft -20;
 	
 drawTrackedText(text, startX, baselineY, tracking);
 
